1dda8ea9fe218a384a189dbcc4ccfe4229b8805e
Cory Powell <coryrpowell@gmail.com>
2023-08-08 11:18:08 -0400

    add vendor and item classes and initialize tests

---
+++ spec/vendor_spec.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+  before do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+
+  end
+
+  describe "#initialize" do
+    it "exists and has readable attributes" do
+      expect(@item1).to be_a(Item)
+      expect(@item2).to be_a(Item)
+      expect(@vendor).to be_a(Vendor)
+
+    end
+  end
+
+end
